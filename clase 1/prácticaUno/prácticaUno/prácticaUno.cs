eed60d7dfcfcef56231d31ce39552f50791abfe3
AbigailSantillan <60194394+AbigailSantillan@users.noreply.github.com>
2020-01-27 13:30:38 -0600

    practica1

---
+++ clase 1/prácticaUno/prácticaUno/prácticaUno.cs
@@ -0,0 +1,12 @@
+using System;
+namespace practicauno
+{
+	class program
+	{
+        string nombre = "Mariana";
+		static void Main(string[] args)
+        {
+            Console.WriteLine($"Hola {args[0]} {args[0]}");
+        }
+	}
+}
