24c61481993e9d478802ab583cae86f12978b772
AbigailSantillan <60194394+AbigailSantillan@users.noreply.github.com>
2020-01-31 14:09:27 -0600

    Practica 1

---
+++ clase 1/practica1/practica1/Program.cs
@@ -0,0 +1,15 @@
+﻿using system;
+//libreria principal
+
+namespace practica1
+{
+    class program //program es una clase base
+    {
+        static string nombre = "Abigail"; //Declaración de una variable 
+
+        static void Main(string[] args) //args = Es un argumento, siempre van entre comillas ""
+        {
+            Console.WriteLine($"Hello {args[0]} {args[1]}");
+        }
+    }
+}
