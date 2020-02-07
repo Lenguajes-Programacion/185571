c081410d8660128425e7f56a800242e13682f26d
AbigailSantillan <60194394+AbigailSantillan@users.noreply.github.com>
2020-02-07 13:51:57 -0600

    practica2

--- clase 1/PracticaUno/ProyectoDos/Program.cs
+++ clase 1/PracticaUno/ProyectoDos/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-//calculadora 
+//calculadora //tarea, operaciones basicas 
 //Se reciben dos argumentos valor1 (int), valor2 (string) operador
 //valor3 (int)
 //responde el resultado (int)
@@ -10,11 +10,19 @@ namespace ProyeyctoDos
     {
         static void Main(string[] args)
         {
+            Console.Title = "MiniCalculadora";
+            Console.WriteLine("Calculadora");
+            int v1 = 0, v2 = 0, R = 0;
+            Console.WriteLine("Eliga una opcion");
+            resp = Console.ReadLine(); 
+              
             switch (args[1])
             {
                 case "+":
-                    Console.WriteLine("Es una suma");
-                    break;
+                    Console.WriteLine("Eligío sumar");
+                    R = v1 + v2;
+                    Console.WriteLine("{0} + {1}= {2}", v1, v2, R); 
+                        break;
                 default:
                     Console.WriteLine("Los argumentos no cuentan con un operador valido" + args[1]);
                     break;
