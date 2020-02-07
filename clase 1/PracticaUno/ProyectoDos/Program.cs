ad54aa4fe7568e7383cd1d2f3ecc170406e0cf9a
AbigailSantillan <60194394+AbigailSantillan@users.noreply.github.com>
2020-02-07 13:21:35 -0600

    practicaDos

---
+++ clase 1/PracticaUno/ProyectoDos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//calculadora 
+//Se reciben dos argumentos valor1 (int), valor2 (string) operador
+//valor3 (int)
+//responde el resultado (int)
+//se busca siempre darle una respuesta al usuario, siendo interactivo. 
+namespace ProyeyctoDos
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (args[1])
+            {
+                case "+":
+                    Console.WriteLine("Es una suma");
+                    break;
+                default:
+                    Console.WriteLine("Los argumentos no cuentan con un operador valido" + args[1]);
+                    break;
+            }
+        }
+    }
+}
---
+++ clase 1/PracticaUno/proyectoUno/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace proyectoUno
+{ 
+    class Program
+    {
+        static string nombre = "Abigail";
+        static void Main(string[] args)
+        {
+            if (args[0].Length > 0 && args[0] == nombre)//es un dato booleano true or false
+            {
+                //"&&" Condición and 
+                //Condición or "||"
+                //Tipos de comparación:">" Mayor "<" Menos "=="Igual ">="Mayor igual "<="Menor igual "!="Diferente que "!"
+                Console.WriteLine($"Hello {args[0]}  {args[1]}");
+            }
+            else //es un sino 
+            {
+                Console.WriteLine("User unauthorized");
+            }
+        }
+    }
+}
