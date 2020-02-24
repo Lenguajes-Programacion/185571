401a3e9dbad00011fd302bbc220101502b69d920
AbigailSantillan <60194394+AbigailSantillan@users.noreply.github.com>
2020-02-24 13:46:05 -0600

    memoria

---
+++ prácticas/proyecto2v2/proyecto2v2/Memoria.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO; 
+using System.Text;
+
+namespace proyecto2v2
+{
+    class Memoria
+    {
+        public void LeerMemoria()
+        {
+            string archivoDB = "../../../db.json"; //Es la ubicación del archivo
+            StreamReader reader = new StreamReader(archivoDB); //es para que lea en tiempo real el archivo 
+            var dbJSON = reader.ReadToEnd();
+            var dbObjet = JObject.parse(dbJSON); 
+            foreach ((var key, var item) in dbObjet)
+            {
+                Console.WriteLine("Dato en memoria:");
+                MemoriaData memoriaData = new MemoriaData(DateTime.Now, item["operacion"].ToString(), (int)item["resultado"]);
+                Console.WriteLine(key.ToString());
+                Console.WriteLine(MemoriaData.resultado.ToString()); 
+            }
+        }
+    }
+    class MemoriaData
+    {
+        public DateTime fecha;
+        public string operacion;
+        public int resultado; 
+        public MemoriaData (DateTime date )
+    }
+}
